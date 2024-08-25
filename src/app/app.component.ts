5486e12966bd2f0f8e71e0e07c73862b263b418e
Kengi Ortega <kortega90@hotmail.com>
2024-08-25 20:34:55 -0300

    formulario implement, format currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { EgresoServicio } from './egreso/egreso.servicio';
 import { IngresoServicio } from './ingreso/ingreso.servicio';
 import { Ingreso } from './ingreso/ingreso.model';
@@ -7,43 +7,42 @@ import { Egreso } from './egreso/egreso.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  ingresos: Ingreso[] = [];
-  egresos: Egreso[] = [];
-
-  ingresoTotal: number = 0;
-  egresoTotal: number = 0;
-  presupuestoTotal: number = 0;
+export class AppComponent {
+  ingresos:Ingreso[]=[];
+  egresos:Egreso[]=[];
 
   constructor(
     private ingresoServicio: IngresoServicio,
     private egresoServicio: EgresoServicio
-  ) {}
-
-  ngOnInit(): void {
-    this.ingresos = this.ingresoServicio.ingresos;
-    this.egresos = this.egresoServicio.egresos;
-
-    this.ingresoTotal = this.calcularIngresoTotal();
-    this.egresoTotal = this.calcularEgresoTotal();
-    this.presupuestoTotal = this.calcularPresupuestoTotal();
+  ){
+    this.ingresos = ingresoServicio.ingresos;
+    this.egresos = egresoServicio.egresos;
   }
 
-  calcularIngresoTotal(): number {
-    return this.ingresos.reduce((total, ingreso) => total + ingreso.valor, 0);
+  getIngresoTotal(){
+    let ingresoTotal:number=0;
+    this.ingresos.forEach(ingreso =>{
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal;
   }
 
-  calcularEgresoTotal(): number {
-    return this.egresos.reduce((total, egreso) => total + egreso.valor, 0);
+  getEgresoTotal(){
+    let egresoTotal:number=0;
+    this.egresos.forEach(egreso =>{
+      egresoTotal += egreso.valor;
+    });
+    return egresoTotal;
   }
 
-  calcularPresupuestoTotal(): number {
-    return this.ingresoTotal - this.egresoTotal;
+  getPorcentajeTotal(){
+    return this.getEgresoTotal()/this.getIngresoTotal();
   }
 
-  getPorcentajeTotal(): number {
-    return this.ingresoTotal > 0 ? (this.egresoTotal / this.ingresoTotal) * 100 : 0;
+  getPresupuestoTotal(){
+    return this.getIngresoTotal() - this.getEgresoTotal();
   }
+
 }
